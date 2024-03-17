ec218d84dfdfc08ca0c5dad37a1de8aafda335f0
Ben Rozhon <benrozhon7@gmail.com>
2024-03-17 02:07:58 -0500

    grass

--- km/src/main.cpp
+++ km/src/main.cpp
@@ -23,10 +23,42 @@ namespace driver {
 
 		constexpr ULONG write = CTL_CODE(FILE_DEVICE_UNKNOWN, 0x698, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
 	}
+
+	struct Request {
+		HANDLE process_id;
+
+		PVOID target;
+		PVOID buffer;
+
+		SIZE_T size;
+		SIZE_T return_size;
+	};
+
 } // Namespace Driver
+
+NTSTATUS driver_main(PDRIVER_OBJECT driver_object, PUNICODE_STRING registry_path) {
+	UNREFERENCED_PARAMETER(registry_path);
+
+	UNICODE_STRING device_name = {};
+	RtlInitUnicodeString(&device_name, L"\\Driver\\KernelDriver");
+
+	PDEVICE_OBJECT device_object = nullptr;
+	NTSTATUS status = IoCreateDevice(driver_object, 0, &device_name, FILE_DEVICE_UNKNOWN, FILE_DEVICE_SECURE_OPEN, FALSE, &device_object);
+
+	if (status != STATUS_SUCCESS)
+	{
+		debug_print("[-] Failed to create driver device.\n");
+		return status;
+	}
+
+	return STATUS_SUCCESS;
+}
 
 NTSTATUS DriverEntry() {
 	debug_print("[+] Woah this shit is nuts dude no way");
 
-	return STATUS_SUCCESS;
+	UNICODE_STRING driver_name = {};
+	RtlInitUnicodeString(&driver_name, L"\\Driver\\KernelDriver");
+
+	return IoCreateDriver(&driver_name, &driver_main);
 }
